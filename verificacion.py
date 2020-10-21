f6a4088d3eca41bf6f9dcdfd224ee8c25c198577
ACLXRD <andres.lopez01@correo.usa.edu.co>
2020-10-20 22:50:04 -0500

    Se añaden archivos funcionando

---
+++ verificacion.py
@@ -0,0 +1,13 @@
+import cgi
+import re
+data = cgi.FieldStorage()
+print('<h1>Verificacion de Correo</h1>')
+def correoValido(correo):
+    valida = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+    return re.match(valida, correo) is not None
+# correo = data.getvalue('correo')
+correo=input('Ingresa tu correo: ')
+if correoValido(correo):
+    print('<h2>La direccion de email " {} " es correcta.</h2>'.format(correo))
+else:
+    print('<h2>La direccion de email es incorrecta.</h2>')
